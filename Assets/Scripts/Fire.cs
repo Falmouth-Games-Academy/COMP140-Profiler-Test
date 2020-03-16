de3e92a6b40d8b855cede7581e184c389d04c612
Brian McDonald <brian.mcdonald@falmouth.ac.uk>
2020-03-16 07:40:12 +0000

    Added Test Scene

---
+++ Assets/Scripts/Fire.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    [SerializeField]
+    Transform bulletSpawnPoint;
+
+    [SerializeField]
+    GameObject bulletPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate<GameObject>(bulletPrefab, bulletSpawnPoint.position, Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/Scripts/Missile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Missile : MonoBehaviour
+{
+    Rigidbody rb;
+    
+
+    [SerializeField]
+    private Vector3 direction=Vector3.forward;
+
+    [SerializeField]
+    private float speed=10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = direction * speed;
+    }
+}
